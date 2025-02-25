2fd8ad594b944806952cdc1f943db07eacbc038f
tanukiti <shelly.ai800@gmail.com>
2025-02-26 03:05:48 +0900

    3-3

---
+++ AOJ_ITP/ITP3-3.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    int x, y;
+    for (int i = 1;; i++) {
+        cin >> x >> y;
+        if (x == 0 && y == 0)break;
+        if (x <= y) {
+            cout << x << " " << y << endl;
+        }
+        else {
+            cout << y << " " << x << endl;
+        }
+    }
+}
